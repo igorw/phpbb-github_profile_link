af349bc7a02464b0a4acd3820f3db6f811285fe3
Igor Wiedler <igor@ig-mac.local>
2010-02-24 20:49:57 +0100

    a bit of documentation to the build and validate scripts

--- develop/build.sh
+++ develop/build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
+# build a mod package using git-archive
+#
+# develop/build.sh [VERSION]
+#
+# must specify files and folders manually because git-archive fails to respect
+# gitattributes
+
 VERSION="dev"
 if [ $# -gt 0 ];
 then
 	VERSION=$1
 fi
+
 git archive --format=zip --prefix=github_profile_link/ HEAD install.xml license.txt modx.prosilver.en.xsl root > github_profile_link-$VERSION.zip
--- develop/validate.sh
+++ develop/validate.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+# validate install.xml against the modx xsd schema
+#
+# depends on xmllint
+
 xmllint --noout --schema develop/modx-schema.xsd install.xml

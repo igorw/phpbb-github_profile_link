73e78d9df65dcfcddfae19d7081e086ba04f8135
Igor Wiedler <igor@ig-mac.local>
2010-02-24 22:04:12 +0100

    create a full-blown build script

--- develop/build.sh
+++ develop/build.sh
@@ -1,15 +1,38 @@
 #!/bin/bash
-# build a mod package using git-archive
+# build a mod package
 #
 # develop/build.sh [VERSION]
 #
+# depends on: git-archive, svn-export, zip
+#
 # must specify files and folders manually because git-archive fails to respect
 # gitattributes
+#
+# make sure you are on the master branch when building
 
+# set version to $1, default to "dev"
 VERSION="dev"
 if [ $# -gt 0 ];
 then
 	VERSION=$1
 fi
 
-git archive --format=zip --prefix=github_profile_link/ HEAD install.xml license.txt modx.prosilver.en.xsl root > github_profile_link-$VERSION.zip
+ZIP_NAME="github_profile_link-$VERSION.zip"
+PREFIX="github_profile_link"
+
+git archive --format=zip --prefix=$PREFIX/ -o $ZIP_NAME HEAD install.xml license.txt modx.prosilver.en.xsl root
+
+# fetch umil
+UMIL_DIR="$PREFIX/root/umil"
+if [ ! -d $UMIL_DIR ];
+then
+	svn export --quiet http://code.phpbb.com/svn/umil/tags/release_$(cat develop/UMIL_VERSION | sed 's/\./_/g')/root/umil $UMIL_DIR
+	for file in $(ls "$UMIL_DIR/language")
+	do
+		if [ "$(basename $file)" != "en" ];
+		then
+			rm -rf "$UMIL_DIR/language/$file"
+		fi
+	done
+	zip -gr $ZIP_NAME $PREFIX
+fi
